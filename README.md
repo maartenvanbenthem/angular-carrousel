a379c08bd9d130cb6d890963c593da52717013f7
Maarten van Benthem <maarten@damsko-media.nl>
2014-12-18 16:27:45 +0100

    Cleanup and added todo in the readme

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ angular-carrousel
 =================
 
 A lazy loading image carrousel
+
+## Todo :
+ - ~~basic setup~~
+ - create the carrousel directive
+ 	- carrousel mechanism 
+ - lazyloading
+ - unit tests in karma
+ - end-to-end tests in protractor
+ - bower packaging
+
--- src/controllers/CarrouselController.js
+++ src/controllers/CarrouselController.js
@@ -1,42 +1,12 @@
 appControllers.controller('CarrouselCtrl', ['$scope', 
 	
 	function ($scope) {
-			
-		console.log("CarrouselCtrl initiated");
-		
+				
 		$scope.imageData = [];
 		
 		generateTestImageData($scope.imageData,20);
 		
 		console.log($scope.imageData.length);
-		
-		$scope.users = [
-                      {
-                          name:"Mahesh",
-                          description:"A geek",
-                          age:"22"
-                      },
-                      {
-                          name:"Ganesh",
-                          description:"A nerd",
-                          age:"25"
-                      },
-                      {
-                          name:"Ramesh",
-                          description:"A noob",
-                          age:"27"
-                      },
-                      {
-                          name:"Ketan",
-                          description:"A psychopath",
-                          age:"26"
-                      },
-                      {
-                          name:"Niraj",
-                          description:"Intellectual badass",
-                          age:"29"
-                      }
-                    ];
 		
 		function generateTestImageData(imageDataObject,length) {
 		
